cf9b1b99ea0e52fe1d0390e095891f9c69abfd39
Mark Seftel <mark@iMac.localdomain>
2026-04-19 11:37:01 +0200

    debug: add session debug endpoint

---
+++ apps/web/src/routes/api/debug-session/+server.ts
@@ -0,0 +1,19 @@
+import { json } from '@sveltejs/kit'
+import { getActualUserId, getUserIdFromRequest } from '$lib/server/permissions'
+
+export const GET = async ({ cookies, locals }) => {
+  const allCookies = cookies.getAll()
+  const authCookies = allCookies.filter((c: any) => c.name.includes('sb-') || c.name.includes('auth'))
+
+  const session = await locals.getSession()
+  const actualUserId = await getActualUserId(cookies)
+  const effectiveUserId = await getUserIdFromRequest(cookies)
+
+  return json({
+    cookieNames: authCookies.map((c: any) => ({ name: c.name, valueLength: c.value?.length, valueStart: c.value?.substring(0, 30) })),
+    sessionUserId: session?.user?.id ?? null,
+    sessionEmail: session?.user?.email ?? null,
+    actualUserId,
+    effectiveUserId
+  })
+}
